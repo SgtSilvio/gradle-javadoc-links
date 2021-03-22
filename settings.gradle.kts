8809366142956f55439a28b9d7a1069dcdd83bf1
Silvio Giebl <silvio.giebl@hivemq.com>
2021-03-23 00:27:13 +0100

    Cleanup settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
+rootProject.name = "gradle-javadoc-links"
+
 pluginManagement {
     plugins {
         kotlin("jvm") version "${extra["plugin.kotlin.version"]}"
         id("com.gradle.plugin-publish") version "${extra["plugin.plugin-publish.version"]}"
     }
 }
-
-rootProject.name = "gradle-javadoc-links"
