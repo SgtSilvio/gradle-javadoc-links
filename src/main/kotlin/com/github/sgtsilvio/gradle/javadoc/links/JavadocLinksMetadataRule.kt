f0b671e202888f0beae9045389170869f6024e54
Silvio Giebl <gieblsilvio@outlook.com>
2021-10-16 20:42:45 +0200

    Simplified check for existing javadocElements variant

--- src/main/kotlin/com/github/sgtsilvio/gradle/javadoc/links/JavadocLinksMetadataRule.kt
+++ src/main/kotlin/com/github/sgtsilvio/gradle/javadoc/links/JavadocLinksMetadataRule.kt
@@ -2,7 +2,6 @@ package com.github.sgtsilvio.gradle.javadoc.links
 
 import org.gradle.api.artifacts.ComponentMetadataContext
 import org.gradle.api.artifacts.ComponentMetadataRule
-import org.gradle.api.artifacts.ModuleVersionIdentifier
 import org.gradle.api.attributes.Bundling
 import org.gradle.api.attributes.Category
 import org.gradle.api.attributes.DocsType
@@ -10,6 +9,7 @@ import org.gradle.api.attributes.Usage
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.kotlin.dsl.named
+import java.util.concurrent.atomic.AtomicBoolean
 import javax.inject.Inject
 
 /**
@@ -20,20 +20,17 @@ abstract class JavadocLinksMetadataRule : ComponentMetadataRule {
     @get:Inject
     protected abstract val objects: ObjectFactory
 
-    private val modulesWithExistingVariant = mutableSetOf<ModuleVersionIdentifier>()
-
     override fun execute(context: ComponentMetadataContext) {
         context.details.run {
+            val existing = AtomicBoolean()
             withVariant(JavaPlugin.JAVADOC_ELEMENTS_CONFIGURATION_NAME) {
                 attributes {
-                    if (keySet().size > 1) {
-                        modulesWithExistingVariant.add(id)
-                    }
+                    existing.set(true)
                 }
             }
             addVariant("javadocLinkElements") {
                 attributes {
-                    if (!modulesWithExistingVariant.contains(id)) {
+                    if (!existing.get()) {
                         attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.DOCUMENTATION))
                         attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType.JAVADOC))
                         attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
