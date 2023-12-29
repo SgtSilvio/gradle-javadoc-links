a95436b66a2bfe6305381ea20b757f7ae3c74c73
Silvio Giebl <gieblsilvio@outlook.com>
2023-12-29 23:30:39 +0100

    build > use = instead of .set()

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,24 +10,24 @@ group = "io.github.sgtsilvio.gradle"
 description = "Gradle plugin to ease defining Javadoc links"
 
 metadata {
-    readableName.set("Gradle Javadoc Links Plugin")
+    readableName = "Gradle Javadoc Links Plugin"
     license {
         apache2()
     }
     developers {
         register("SgtSilvio") {
-            fullName.set("Silvio Giebl")
+            fullName = "Silvio Giebl"
         }
     }
     github {
-        org.set("SgtSilvio")
+        org = "SgtSilvio"
         issues()
     }
 }
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion = JavaLanguageVersion.of(8)
     }
 }
 
@@ -36,15 +36,15 @@ repositories {
 }
 
 gradlePlugin {
-    website.set(metadata.url)
-    vcsUrl.set(metadata.scm.get().url)
+    website = metadata.url
+    vcsUrl = metadata.scm.get().url
     plugins {
         create("javadocLinks") {
             id = "$group.javadoc-links"
             implementationClass = "$group.javadoc.links.JavadocLinksPlugin"
             displayName = metadata.readableName.get()
             description = project.description
-            tags.set(listOf("javadoc", "links"))
+            tags = listOf("javadoc", "links")
         }
     }
 }
